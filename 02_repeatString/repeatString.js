cbe806a4ef07a9944c3d26ddea549dd03b267629
Ahmed <ahmadadil345@gmail.com>
2024-07-21 18:01:05 +0300

    Make the function pass all the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,17 @@
 const repeatString = function(string, num) {
+    if (num === 0){
+        return ''
+    } else if (num < 0) {
+        return 'ERROR'
+    }
+    
     string2 = string;
     for (i = 1; i < num; i++) {
         string += string2;
-    }
-    return string
+    } return string
+    
+    
+    
 };
 
 // Do not edit below this line
