f6463fe1945e58bab256ad2ac08cd7dc942be58d
Ahmed <ahmadadil345@gmail.com>
2024-07-21 17:33:57 +0300

    Make the function pass the first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    string2 = string;
+    for (i = 1; i < num; i++) {
+        string += string2;
+    }
+    return string
 };
 
 // Do not edit below this line
