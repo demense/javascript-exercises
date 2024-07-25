98dc2598e0958b16c68e96f21295f238f3624109
Ahmed <ahmadadil345@gmail.com>
2024-07-25 18:22:40 +0300

    Pass all test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arg1, ...arg2){
+    arg2 = Array.from(arg2);
+    function isEqualTo(value) {
+        return !(arg2.includes(value));    
+    }
+    filteredArray = arg1.filter(isEqualTo);
+    return filteredArray
+}
+    
 
 // Do not edit below this line
 module.exports = removeFromArray;
