0790af8d0ca701d79b83272473c4874898355045
Ahmed <ahmadadil345@gmail.com>
2024-07-27 09:28:05 +0300

    Pass all the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let nums = [];
+    let sum = 0;
 
+    if ((start < 0) || (end < 0) || !(Number.isInteger(start)) || !(Number.isInteger(end)) || (isNaN(start)) || (isNaN(end))) {
+        return 'ERROR';
+    }
+
+    if (start < end ) { 
+        for (i = start; i <= end; i++) {
+            nums.push(i);
+        }
+    } else if (start > end) {
+        for (i = start; i >= end; i--) {
+            nums.push(i);
+        }
+    }
+
+    for (i = 0; i < nums.length; i++) {
+        sum += nums[i];
+    }
+    return sum;
 };
 
 // Do not edit below this line
