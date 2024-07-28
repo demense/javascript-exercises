ef33624b5945114bbbebf86da9d26e43f43de89e
Ahmed <ahmadadil345@gmail.com>
2024-07-28 17:47:44 +0300

    Pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for (i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  let arr = [];
+  let factorial = 0;
+  for (i = 1; i <= num; i++) {
+    arr.push(i);
+    factorial = arr.reduce((a,b) => a*b);
+  }
+  return factorial;
 };
 
 // Do not edit below this line
