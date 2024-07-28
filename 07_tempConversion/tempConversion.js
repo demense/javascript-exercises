56308108c5fc8baabea15685cafadf126c08c2a1
Ahmed <ahmadadil345@gmail.com>
2024-07-28 16:58:30 +0300

    Pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeFahrenheit) {
+  return parseFloat((((degreeFahrenheit) - 32) * (5/9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeCelsius) {
+  return parseFloat(((degreeCelsius) * (9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
