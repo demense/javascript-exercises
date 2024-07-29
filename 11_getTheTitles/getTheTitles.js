76d28b76978bb6dc161567ff5ccdb45df78a504b
Ahmed <ahmadadil345@gmail.com>
2024-07-29 09:15:16 +0300

    Pass all the tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    for (i = 0; i < books.length; i++) {
+        if (books[i].title) {
+            titles.push(books[i].title);
+        }
+    }
+    return titles;
 };
 
 // Do not edit below this line
