61a064d8c490bfcd018d7a7e8fd62bd0fb2b4c65
Ahmed <ahmadadil345@gmail.com>
2024-07-29 06:51:43 +0300

    Pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string
+      .toLowerCase()
+      .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+      .replace(/\s{2,}/g, " ")
+      .replace(/\s+/g, "");
+    reversedString = string.split("").reverse().join("");
+    if (string === reversedString) {
+      return true;
+    } else {
+      return false;
+    }
 };
 
 // Do not edit below this line
