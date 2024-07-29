ecd3b44523ab309f9906e132c2869705df254ae5
Ahmed <ahmadadil345@gmail.com>
2024-07-29 08:45:35 +0300

    Pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+    if (nth < 0) return "OOPS";
+    parseInt(nth);
+    let fiboSeq = [0,1];
+    for (i = 0; i < nth; i++) {
+        sum = fiboSeq[i] + fiboSeq[i+1];
+        fiboSeq.push(sum);
+    }
+    return fiboSeq[nth];
 };
 
 // Do not edit below this line
