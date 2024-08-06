4e03b1a75d4e2ebb4299bfb9f81f0a7823cec0c6
Ahmed <ahmedadil9102@gmail.com>
2024-08-06 10:29:02 -0400

    Pass all the tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    people.map(a => {if (a.yearOfDeath === undefined){return a.yearOfDeath = currentYear}});
+    let ages = people.map(a => a.yearOfDeath - a.yearOfBirth);
+    let oldest = ages.indexOf(Math.max(...ages));
+    return people[oldest];
 };
 
 // Do not edit below this line
